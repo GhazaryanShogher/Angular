4ef2957324670504d81012560af6d7fc4c4e6e6d
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-07-17 00:59:04 +0400

    online shop

--- firstProject/src/app/app.module.ts
+++ firstProject/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
